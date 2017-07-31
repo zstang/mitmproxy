5ca9c0922f45499a30a3aba498c4b532e2f4f0ff
Maximilian Hils <git@maximilianhils.com>
2017-07-31 22:12:18 +0200

    properly import mitmproxy.proxy

--- mitmproxy/proxy/__init__.py
+++ mitmproxy/proxy/__init__.py
@@ -1,4 +1,3 @@
-"""
 from .config import ProxyConfig
 from .root_context import RootContext
 from .server import ProxyServer, DummyServer
@@ -8,4 +7,3 @@ __all__ = [
     "ProxyConfig",
     "RootContext"
 ]
-"""
